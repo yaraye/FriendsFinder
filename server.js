1461a40ab0bf249fd7a8961b031efb063937ee5a
Yaraye <yaraye@gmail.com>
2018-08-09 16:52:31 -0400

    updates

--- server.js
+++ server.js
@@ -3,3 +3,21 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
+var PORT = process.env.PORT;
+
+// Expose the public directory to access CSS files
+app.use(express.static(path.join(__dirname, './app/public')));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+// Add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+//listening on PORT
+app.listen(PORT, function() {
+  console.log('friendFinder app is listening on PORT: ' + PORT);
+});
